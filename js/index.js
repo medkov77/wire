775501eae9f7eba5fcccedfc034db2bf14a55fc5
medkov77 <medkov77@mail.rum>
2023-03-12 17:18:10 +0300

    anhcors

--- js/index.js
+++ js/index.js
@@ -32,3 +32,17 @@ const swiper = new Swiper(".swiper", {
 
   // And if we need scrollbar
 });
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").slice(1);
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
